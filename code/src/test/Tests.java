9206f755a9d7fc7da4b665b84c3df41ef55e0d59
jj203 <acealexzia@gmail.com>
2025-10-27 20:35:13 +0000

    Finish Implement Test cases

--- code/src/test/Tests.java
+++ code/src/test/Tests.java
@@ -3,6 +3,7 @@ package test;
 import exceptions.LaneCodeAlreadyInUseException;
 import exceptions.LaneCodeNotRegisteredException;
 import exceptions.ProductUnavailableException;
+import impl.ProductRecord;
 import impl.VendingMachine;
 import impl.VendingMachineProduct;
 import org.junit.jupiter.api.BeforeAll;
@@ -63,6 +64,14 @@ public class Tests {
         vendingMachine.registerProduct(product5);
     }
     @Test
+    public void countNumberOfProductsTestSuccess() throws LaneCodeAlreadyInUseException {
+        vendingMachine.registerProduct(product1);
+        vendingMachine.registerProduct(product2);
+        vendingMachine.registerProduct(product3);
+        final int expected = 3;
+        assertEquals(expected, vendingMachine.getNumberOfProducts());
+    }
+    @Test
     public void unRegisterProductTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException {
         vendingMachine.registerProduct(product1);
         vendingMachine.registerProduct(product2);
@@ -91,7 +100,7 @@ public class Tests {
         vendingMachine.unregisterProduct(product6);
     }
     @Test
-    public void addItemSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException {
+    public void addItemTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException {
         vendingMachine.registerProduct(product1);
         vendingMachine.registerProduct(product2);
         vendingMachine.addItem("00");
@@ -102,13 +111,13 @@ public class Tests {
 
     }
     @Test
-    public void addItemFail() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException {
+    public void addItemTestFail() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException {
         vendingMachine.registerProduct(product1);
         vendingMachine.registerProduct(product2);
         vendingMachine.addItem("06");
     }
     @Test
-    public void buyItemSaleCountSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+    public void buyItemSaleCountTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
         vendingMachine.registerProduct(product1);
         vendingMachine.addItem("00");
         vendingMachine.addItem("00");
@@ -117,7 +126,26 @@ public class Tests {
         final int expected = 2;
         assertEquals(expected, vendingMachine.getNumberOfSales("00"));
     }
+    @Test
+    public void buyItemSaleCountTestFail() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+        vendingMachine.registerProduct(product1);
+        vendingMachine.addItem("00");
+        vendingMachine.addItem("00");
+        vendingMachine.buyItem("00");
+        vendingMachine.buyItem("00");
+        vendingMachine.getNumberOfSales("10");
+    }
 
+    @Test
+    public void buyItemDecreaseCountTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+        vendingMachine.registerProduct(product1);
+        vendingMachine.addItem("00");
+        vendingMachine.addItem("00");
+        vendingMachine.buyItem("00");
+        vendingMachine.buyItem("00");
+        final int expected = 0;
+        assertEquals(expected, vendingMachine.getTotalNumberOfItems());
+    }
     @Test
     public void buyItemTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
         vendingMachine.registerProduct(product1);
@@ -127,7 +155,6 @@ public class Tests {
         final int expected = 1;
         vendingMachine.buyItem("00");
         assertEquals(expected, vendingMachine.getNumberOfItems("00"));
-        assertEquals(expected, vendingMachine.getTotalNumberOfItems());
     }
     @Test
     public void countAfterBuyTotalItemTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
@@ -156,16 +183,72 @@ public class Tests {
         vendingMachine.buyItem("00");
     }
     @Test
-    public void getNumberOfItemFail() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+    public void getNumberOfItemByCodeTestSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+        vendingMachine.registerProduct(product1);
+        vendingMachine.registerProduct(product2);
+        vendingMachine.registerProduct(product3);
+        vendingMachine.addItem("00");
+        vendingMachine.buyItem("01");
+        vendingMachine.buyItem("01");
+        vendingMachine.getNumberOfItems("02");
+        vendingMachine.getNumberOfItems("02");
+        vendingMachine.getNumberOfItems("02");
+        final int expected = 3;
+        assertEquals(expected, vendingMachine.getNumberOfItems("02"));
+    }
+    @Test
+    public void getNumberOfItemByCodeTestFail() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
         vendingMachine.registerProduct(product1);
         vendingMachine.addItem("00");
         vendingMachine.buyItem("00");
         vendingMachine.getNumberOfItems("05");
     }
 
-   @Test
+    @Test
+    public void getNumberOfAllItemSuccessTest() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+        vendingMachine.registerProduct(product1);
+        vendingMachine.registerProduct(product2);
+        vendingMachine.registerProduct(product3);
+        vendingMachine.addItem("00");
+        vendingMachine.addItem("00");
+        vendingMachine.addItem("00");
+        vendingMachine.addItem("01");
+        vendingMachine.addItem("02");
+        final int expected = 5;
+        assertEquals(expected, vendingMachine.getTotalNumberOfItems());
+    }
+    @Test
+    public void getMostPopularSuccess() throws LaneCodeAlreadyInUseException, LaneCodeNotRegisteredException, ProductUnavailableException {
+        vendingMachine.registerProduct(product1);
+        vendingMachine.registerProduct(product2);
+        vendingMachine.registerProduct(product3);
+        vendingMachine.addItem("00");
+        vendingMachine.addItem("01");
+        vendingMachine.addItem("02");
+        vendingMachine.addItem("02");
+        vendingMachine.addItem("02");
+        vendingMachine.buyItem("01");
+        vendingMachine.buyItem("02");
+        vendingMachine.buyItem("02");
+        vendingMachine.buyItem("02");
+        final String expected = "02";
+        assertEquals(expected, vendingMachine.getMostPopular().getLaneCode());
+    }
+    @Test
     public void vendingMachineProductNotNull() {
         IVendingMachineProduct vendingMachineProduct = Factory.getInstance().makeVendingMachineProduct("A1", "Haggis Crisps");
         assertNotNull(vendingMachineProduct);
+    }
+    @Test
+    public void vendingMachineNull() {
+        IVendingMachine vendingMachine1 = Factory.getInstance().makeVendingMachine();
+        assertNotNull(vendingMachine1);
+    }
+
+    @Test
+    public void productNotNull() {
+        IVendingMachineProduct vendingMachineProduct = Factory.getInstance().makeVendingMachineProduct("A1", "Haggis Crisps");
+        IProductRecord productRecord = Factory.getInstance().makeProductRecord(vendingMachineProduct);
+        assertNotNull(productRecord);
     }
 }
